054a26b0d2820d7d83910e0397b9b7c4348de028
Ishaan <ishaanshetty12@gmail.com>
2022-06-01 09:48:00 +0530

    Files modified

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButton = document.getElementById('data-number');
+const operationButton = document.getElementById('data-operation');
+const equalsButton = document.getElementById('data-equals');
+const deleteButton = document.getElementById('data-delete');
+const allClearButton = document.getElementById('data-all-clear');
+const previousOperandTextElement = document.getElementById('data-previous-operand');
+const currentOperandTextElement = document.getElementById('data-current-operand');
